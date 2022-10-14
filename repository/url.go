d4d6e7f4989b88b7d954099f5fd91034b0c6c1e9
Mohammad AlAli <m@alali.dev>
2022-10-14 13:52:06 +0300

    add FindByCode to UrlRepository

--- repository/url.go
+++ repository/url.go
@@ -8,35 +8,37 @@ import (
 type UrlRepository interface {
 	Create(url *entity.Url) error
 
-	Get(id uint64) (entity.Url, error)
+	Get(id uint64) (*entity.Url, error)
 
 	Update(url *entity.Url) error
 
 	Delete(id uint64) error
 
-	GetAll() ([]entity.Url, error)
+	GetAll() (*[]entity.Url, error)
+
+	FindByCode(code string) (*entity.Url, error)
 }
 
 type UrlRepositoryImpl struct {
 	db *gorm.DB
 }
 
-func (r UrlRepositoryImpl) Get(id uint64) (entity.Url, error) {
+func (r UrlRepositoryImpl) Get(id uint64) (*entity.Url, error) {
 	var url entity.Url
 	tx := r.db.Where("id = ?", id).First(url)
 	if tx.Error != nil {
-		return entity.Url{}, tx.Error
+		return &entity.Url{}, tx.Error
 	}
-	return url, nil
+	return &url, nil
 }
 
-func (r UrlRepositoryImpl) GetAll() ([]entity.Url, error) {
+func (r UrlRepositoryImpl) GetAll() (*[]entity.Url, error) {
 	var urls []entity.Url
 	tx := r.db.Find(urls)
 	if tx.Error != nil {
-		return []entity.Url{}, tx.Error
+		return &[]entity.Url{}, tx.Error
 	}
-	return urls, nil
+	return &urls, nil
 }
 
 func (r UrlRepositoryImpl) Create(url *entity.Url) error {
@@ -52,4 +54,13 @@ func (r UrlRepositoryImpl) Update(url *entity.Url) error {
 func (r UrlRepositoryImpl) Delete(id uint64) error {
 	tx := r.db.Unscoped().Delete(&entity.Url{}, id)
 	return tx.Error
+}
+
+func (r UrlRepositoryImpl) FindByCode(code string) (*entity.Url, error) {
+	var url entity.Url
+	tx := r.db.Where("code = ?", code).First(&url)
+	if tx.Error != nil {
+		return &entity.Url{}, tx.Error
+	}
+	return &url, nil
 }
